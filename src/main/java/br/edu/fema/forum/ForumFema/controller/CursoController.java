ad526641f345e2c60767a622d24de9d0c27946cc
Isabela Ferreira <isabela.ferreiira16@gmail.com>
2024-04-15 19:39:56 -0300

    paginação de cursos e reformulação de código

--- src/main/java/br/edu/fema/forum/ForumFema/controller/CursoController.java
+++ src/main/java/br/edu/fema/forum/ForumFema/controller/CursoController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -32,10 +33,7 @@ public class CursoController {
     private CursoRepository cursoRepository;
 
     @GetMapping
-    public Page<CursoDto> lista(@RequestParam int pagina,
-                                @RequestParam int qtde,
-                                @RequestParam String ordenacao){
-        Pageable paginacao = PageRequest.of(pagina, qtde, Sort.Direction.ASC, ordenacao);
+    public Page<CursoDto> lista(@PageableDefault(sort = "id", direction = Sort.Direction.DESC, page = 0, size = 10) Pageable paginacao){
 
         Page<Curso> cursos = cursoRepository.findAll(paginacao);
         return CursoDto.converter(cursos);
